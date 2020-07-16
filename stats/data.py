a3dc2d2c66649f9380ff4f077eaa62fcaef96042
Whit <may.whitney21@gmail.com>
2020-07-16 17:47:11 -0600

    Update data.py

--- stats/data.py
+++ stats/data.py
@@ -2,23 +2,26 @@ import os
 import glob
 import pandas as pd
 import numpy as np
-os.chdir('../games/')
-game_files = glob.glob('*.EVE')
+
+
+game_files = glob.glob(os.path.join(os.getcwd(), 'games', '*.EVE'))
 list.sort(game_files)
-counter = 0
-game_frames=[]
-while counter < len(game_files):
-    path = game_files[counter]
-    names = ['type','multi2','multi3','multi4','multi5','multi6','event']
-    game_frame = pd.read_csv(path, names = names)
+
+game_frames= []
+for game_file in game_files:
+    game_frame = pd.read_csv(game_file, names = ['type','multi2','multi3','multi4','multi5','multi6','event'])
     game_frames.append(game_frame)
-    counter = counter + 1
-games = pd.concat(game_frames, ignore_index = True)
-games['multi5'] = games['multi5'].replace("??", np.NaN)
+
+games = pd.concat(game_frames)
+
+games.loc[games['multi5'] == '??', ['multi5']] = ''
+
 identifiers = games['multi2'].str.extract(r'(.LS(\d{4})\d{5})')
 identifiers = identifiers.fillna(method='ffill')
 identifiers.columns=(['game_id','year'])
+
 games = pd.concat([games, identifiers], axis=1, sort=False)
 games = games.fillna('')
 games.loc[:, 'type'] = pd.Categorical(games.loc[:, 'type'])
+
 print (games.head())
